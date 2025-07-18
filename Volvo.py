beddb438395b17d3acebda4119681772c9262ea3
abhayy9 <159525535+abhayy9@users.noreply.github.com>
2025-07-18 08:25:44 +0530

    Update Volvo.py

--- Volvo.py
+++ Volvo.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.head("Welcome to first streamlit ")
+st.header("Welcome to first streamlit ")
 name=st.text_input("enter yur name")
 button=st.button("enter",on_click=lambda: st.write(f"your name is {name}"))
